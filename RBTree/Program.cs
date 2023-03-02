ad4a662115ebdcce76913fc8819063e1f9c19376
Apocrifon <astakhov.stepan2003@gmail.com>
2023-03-03 00:33:59 +0500

    add funk

--- RBTree/Program.cs
+++ RBTree/Program.cs
@@ -1,61 +1,20 @@
-﻿using System;
-
+﻿
 namespace RBTree
 {
-    public enum Colors
-    {
-        Red,
-        Black
-    }
-
-    public class Tree
-    {
-        public Node root = null;
-        public Node Root { get; set; }
-
-        public void Insert(Node node)
-        {
-            var curNode = root;
-            while (curNode!=null)
-                curNode = curNode.Key > node.Key ? curNode.Right : curNode.Left;
-            curNode = new Node(node.Key);
-            Balanced();
-        }
-
-        public void Balanced()
-        {
-
-        }
-
-
-
-
-        // идея вывода дерева через очередь(Ulearn)
-    }
-
-    public class Node   
-    {
-        public int Key { get; set; }
-        public Colors Color { get; set; }
-        public Node Parent { get; set;}
-        public Node Left { get; set; }
-        public Node Right { get; set; }
-
-        public Node(int key)
-        {
-            Key = key;
-            Color = Colors.Red;
-            Parent = null;
-            Left = null;
-            Right = null;
-        }    
-    } 
-
+    
     public class Program
     {
         static void Main()
         {
             var test1 = new Tree();
+            test1.Insert(2);
+            test1.Insert(1);
+            test1.Insert(3);
+            test1.Insert(4);
+            test1.Insert(5);
+            test1.Insert(6);
+            test1.Print();
+            Console.WriteLine(test1.Deep);
         }
     }
 }
---
+++ RBTree/Tree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RBTree
+{
+    public enum Colors
+    {
+        Red,
+        Black
+    }
+
+    public class Tree
+    {
+        private Node root = null;
+        public Node Root { get { return root; } }
+        private int deep;
+        public int Deep { get { return deep; } }
+
+        public void Insert(int value)
+        {
+            var newNode = new Node(value);
+            if (root == null)
+            {
+                root = new Node(value);
+                root.Level = 1;
+                return;
+            }
+            var curNode = root;
+            var prevNode = curNode;
+            while (curNode != null)
+            {
+                prevNode = curNode;
+                curNode = curNode.Key < value ? curNode.Right : curNode.Left;
+            }
+            if (prevNode.Key > value)
+                prevNode.Left = newNode;
+            else
+                prevNode.Right = newNode;
+
+            newNode.Parent = prevNode;
+            newNode.Level = newNode.Parent.Level + 1;
+            deep=newNode.Level;
+            Balanced(newNode);
+        }
+        // TODO
+        public void Delete(int value)
+        {
+
+        }
+        // TODO
+        private void Balanced(Node node)
+        {
+
+        }
+
+        public void Print()
+        {
+            if (root == null)
+                return;
+            var queue = new Queue<Node>();
+            var level = root.Level;
+            queue.Enqueue(root);
+            Console.WriteLine(new string(' ', deep+deep-1)); // fix intervals
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+                if (node.Level > level)
+                {
+                    Console.WriteLine();
+                    level++;
+                }
+                Console.Write(node.Key);
+                Console.Write("  ");
+                if (node.Left != null)
+                    queue.Enqueue(node.Left);
+                if (node.Right != null)
+                    queue.Enqueue(node.Right);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Node
+    {
+        public int Key { get; set; }
+        public Colors Color { get; set; }
+        public Node Parent { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Level { get; set; }  // Field create for print funk
+
+        public Node(int key)
+        {
+            Key = key;
+            Color = Colors.Red;
+            Parent = null;
+            Left = null;
+            Right = null;
+        }
+    }
+}
